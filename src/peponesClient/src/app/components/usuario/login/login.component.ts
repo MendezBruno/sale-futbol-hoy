dc8350b6f3c5938f0a5284ca2c85a86c1b5590c2
Mendez Bruno <bmendez@cideso.mil.ar>
2018-05-28 14:38:59 -0300

    arreglo la altura del flex

--- src/peponesClient/src/app/components/usuario/login/login.component.ts
+++ src/peponesClient/src/app/components/usuario/login/login.component.ts
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  model: any = {};
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  login(model) {
+    console.log(model);
   }
 
 }
